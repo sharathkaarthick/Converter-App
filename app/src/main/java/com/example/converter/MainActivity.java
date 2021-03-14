c0beaa14d5da29d38486a33698a8d857ce1f0465
sharathkaarthick <kaarthick.sk@gmail.com>
2021-03-14 22:58:49 +0530

    Second Update

--- app/src/main/java/com/example/converter/MainActivity.java
+++ app/src/main/java/com/example/converter/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import android.text.InputType;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
